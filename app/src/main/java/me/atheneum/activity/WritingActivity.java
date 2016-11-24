fcff7cc63d8c5b8be113d9c5d953e04620caab36
Sara Montecino <saramontecino@outlook.com>
2016-11-23 17:52:33 -0800

    Spans not applying properly because dumb

--- app/src/main/java/me/atheneum/activity/WritingActivity.java
+++ app/src/main/java/me/atheneum/activity/WritingActivity.java
@@ -3,27 +3,26 @@ package me.atheneum.activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
-import android.inputmethodservice.KeyboardView;
+
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
+
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
-import android.text.SpannedString;
+
 import android.text.style.AbsoluteSizeSpan;
-import android.text.style.RelativeSizeSpan;
+
+import android.text.style.MetricAffectingSpan;
 import android.text.style.StyleSpan;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
+
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
+
 import android.widget.EditText;
-import android.widget.TextView;
+
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -46,15 +45,11 @@ public class WritingActivity extends AppCompatActivity {
     private static final String URL_PUBLISH = "http://104.236.163.131:9000/api/story/publish";
     private static final String PREFS_NAME = "CredentialPrefsFile";
 
-    private static boolean lastCharIsEnter = false;
-
     private static boolean titleIsActive;
     private static boolean subtitleIsActive;
     private static boolean boldIsActive;
     private static boolean italicIsActive;
     private static boolean boldItalicIsActive;
-    private static boolean formattedListIsActive;
-    private static boolean unformattedListIsActive;
 
     private static Toolbar toolbarBottom;
     private EditText bodyTextInput;
@@ -71,16 +66,6 @@ public class WritingActivity extends AppCompatActivity {
             return;
         }
         bodyTextInput = (EditText) findViewById(R.id.editText);
-        bodyTextInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_NULL &&
-                        event.getAction() == KeyEvent.ACTION_DOWN){
-                            System.out.println("Enter key pressed");
-                }
-                return true;
-            }
-        });
         toolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -93,17 +78,18 @@ public class WritingActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.action_format_size:
                         if(titleIsActive){
-                            currentLine = removeTitleSpan(currentLine);
-                            currentLine = addSubtitleSpan(currentLine);
+                            currentLine.removeSpan(AbsoluteSizeSpan.class);
+                            currentLine.setSpan(new AbsoluteSizeSpan(CustomStyles.SUBTITLE), 0, currentLine.length(),Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
                             bodyTextInput.setText(updateTextSpan(bodyText,currentLine,lineBoundaries));
                             setTitleToInactive();
                             setSubtitleToActive();
                         } else if(subtitleIsActive){
-                            currentLine = removeTitleSpan(currentLine);
+                            currentLine.removeSpan(AbsoluteSizeSpan.class);
                             bodyTextInput.setText(updateTextSpan(bodyText,currentLine,lineBoundaries));
                             setSubtitleToInactive();
                         } else{
-                            bodyText.replace(lineBoundaries[0], lineBoundaries[1], addTitleSpan(currentLine));
+                            currentLine.setSpan(new AbsoluteSizeSpan(CustomStyles.TITLE), 0, currentLine.length(),Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+                            bodyText.replace(lineBoundaries[0], lineBoundaries[1], currentLine);
                             bodyTextInput.setText(bodyText);
                             setTitleToActive();
                         }
@@ -111,23 +97,24 @@ public class WritingActivity extends AppCompatActivity {
                         break;
                     case R.id.action_bold:
                         if(boldItalicIsActive){
-                            currentWord = removeStyleSpan(currentWord);
-                            currentWord = addItalicSpan(currentWord);
+                            currentWord.removeSpan(StyleSpan.class);
+                            currentWord.setSpan(new StyleSpan(Typeface.ITALIC), 0, currentWord.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
                             bodyTextInput.setText(updateTextSpan(bodyText,currentWord,wordBoundaries));
                             setBoldItalicToInactive();
                             setBoldToInactive();
                         } else if(boldIsActive){
-                            currentWord = removeStyleSpan(currentWord);
+                            currentWord.removeSpan(StyleSpan.class);
                             bodyTextInput.setText(updateTextSpan(bodyText, currentWord, wordBoundaries));
                             setBoldToInactive();
                         } else if(italicIsActive){
-                            currentWord = removeStyleSpan(currentWord);
-                            currentWord = addBoldItalicSpan(currentWord);
+                            currentWord.removeSpan(StyleSpan.class);
+                            currentWord.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), 0, currentWord.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
                             bodyTextInput.setText(updateTextSpan(bodyText,currentWord,wordBoundaries));
                             setBoldToActive();
                             setBoldItalicToActive();
                         } else{
-                            bodyText.replace(wordBoundaries[0], wordBoundaries[1], addBoldSpan(currentWord));
+                            currentWord.setSpan(new StyleSpan(Typeface.BOLD), 0, currentWord.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+                            bodyText.replace(wordBoundaries[0], wordBoundaries[1], currentWord);
                             bodyTextInput.setText(bodyText);
                             setBoldToActive();
                         }
@@ -135,23 +122,24 @@ public class WritingActivity extends AppCompatActivity {
                         break;
                     case R.id.action_italic:
                         if(boldItalicIsActive){
-                            currentWord = removeStyleSpan(currentWord);
-                            currentWord = addBoldSpan(currentWord);
+                            currentWord.removeSpan(StyleSpan.class);
+                            currentWord.setSpan(new StyleSpan(Typeface.BOLD),0, currentWord.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
                             bodyTextInput.setText(updateTextSpan(bodyText,currentWord,wordBoundaries));
                             setBoldItalicToInactive();
                             setItalicToInactive();
                         } else if(italicIsActive){
-                            currentWord = removeStyleSpan(currentWord);
+                            currentWord.removeSpan(StyleSpan.class);
                             bodyTextInput.setText(updateTextSpan(bodyText, currentWord, wordBoundaries));
                             setItalicToInactive();
                         } else if(boldIsActive){
-                            currentWord = removeStyleSpan(currentWord);
-                            currentWord = addBoldItalicSpan(currentWord);
+                            currentWord.removeSpan(StyleSpan.class);
+                            currentWord.setSpan(new StyleSpan(Typeface.BOLD_ITALIC),0, currentWord.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
                             bodyTextInput.setText(updateTextSpan(bodyText,currentWord,wordBoundaries));
                             setItalicToActive();
                             setBoldItalicToActive();
                         } else{
-                            bodyText.replace(wordBoundaries[0], wordBoundaries[1], addItalicSpan(currentWord));
+                            currentWord.setSpan(new StyleSpan(Typeface.ITALIC),0, currentWord.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+                            bodyText.replace(wordBoundaries[0], wordBoundaries[1], currentWord);
                             bodyTextInput.setText(bodyText);
                             setItalicToActive();
                         }
@@ -287,47 +275,6 @@ public class WritingActivity extends AppCompatActivity {
         return new int[] {stringStartPos,stringEndPos};
     }
 
-    private SpannableStringBuilder addTitleSpan(SpannableStringBuilder text){
-        text.setSpan(new AbsoluteSizeSpan(CustomStyles.TITLE), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return text;
-    }
-
-    private SpannableStringBuilder removeTitleSpan(SpannableStringBuilder text){
-        AbsoluteSizeSpan[] spans = text.getSpans(0,text.length(),AbsoluteSizeSpan.class);
-        if(spans.length != 0) {
-            text.removeSpan(spans[0]);
-        }
-        return text;
-    }
-
-    private SpannableStringBuilder addSubtitleSpan(SpannableStringBuilder text){
-        text.setSpan(new AbsoluteSizeSpan(CustomStyles.SUBTITLE), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return text;
-    }
-
-    private SpannableStringBuilder addItalicSpan(SpannableStringBuilder text){
-        text.setSpan(new StyleSpan(Typeface.ITALIC), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return text;
-    }
-
-    private SpannableStringBuilder addBoldSpan(SpannableStringBuilder text){
-        text.setSpan(new StyleSpan(Typeface.BOLD), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return text;
-    }
-
-    private SpannableStringBuilder addBoldItalicSpan(SpannableStringBuilder text){
-        text.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return text;
-    }
-
-    private SpannableStringBuilder removeStyleSpan(SpannableStringBuilder text){
-        StyleSpan[] spans = text.getSpans(0,text.length(),StyleSpan.class);
-        if(spans.length != 0){
-            text.removeSpan(spans[0]);
-        }
-        return text;
-    }
-
     private SpannableStringBuilder updateTextSpan(SpannableStringBuilder originalText, SpannableStringBuilder newText, int[] boundaries){
         SpannableStringBuilder updatedText = new SpannableStringBuilder();
         /* This gets a bit hacky, but I couldn't figure out a better solution.
@@ -387,14 +334,6 @@ public class WritingActivity extends AppCompatActivity {
         subtitleIsActive = true;
     }
 
-    private static void setUnformattedListToActive(){
-        unformattedListIsActive = true;
-    }
-
-    private static void setFormattedListToActive(){
-        formattedListIsActive = true;
-    }
-
     private static void setBoldToInactive(){
         MenuItem item = toolbarBottom.getMenu().findItem(R.id.action_bold);
         item.setIcon(R.drawable.ic_bold_inactive);
@@ -429,22 +368,12 @@ public class WritingActivity extends AppCompatActivity {
         subtitleIsActive = false;
     }
 
-    private static void setUnformattedListToInactive(){
-        unformattedListIsActive = false;
-    }
-
-    private static void setFormattedListToInactive(){
-        formattedListIsActive = false;
-    }
-
     private static void setAllToInactive(){
         setBoldToInactive();
         setItalicToInactive();
         setBoldItalicToInactive();
         setTitleToInactive();
         setSubtitleToInactive();
-        setFormattedListToInactive();
-        setUnformattedListToInactive();
     }
 
 
@@ -467,7 +396,6 @@ public class WritingActivity extends AppCompatActivity {
 
         @Override
         protected void onSelectionChanged(int selStart, int selEnd) {
-            boolean textChanged = false;
             SpannableString currentLine = getLine(selStart, this);
             SpannableString currentWord = getWord(selStart, this);
             if(selStart == selEnd && selStart != lastCursorPos && selStart != 0) {
@@ -500,8 +428,14 @@ public class WritingActivity extends AppCompatActivity {
                                 }
                             }
                         }
-                        if(wordSpans.length == 0 && lineSpans.length == 0){
-                            setAllToInactive();
+                        if(wordSpans.length == 0){
+                            setBoldItalicToInactive();
+                            setBoldToInactive();
+                            setItalicToInactive();
+                        }
+                        if(lineSpans.length == 0){
+                            setTitleToInactive();
+                            setSubtitleToInactive();
                         }
                     }
                 }
@@ -509,37 +443,16 @@ public class WritingActivity extends AppCompatActivity {
                 //Check to see if Enter is pressed, used to end active formatting states
                 char lastChar = this.getText().charAt(selStart - 1);
                 if(lastChar == '\n'){
+                    SpannableString word = getWord(selStart - 1, this);
+                    MetricAffectingSpan[] spans = word.getSpans(0, currentWord.length(),MetricAffectingSpan.class);
+                    for(MetricAffectingSpan span: spans){
+                        int start = word.getSpanStart(span);
+                        int end = word.getSpanEnd(span);
+                        word.removeSpan(span);
+                        word.setSpan(span,start,end,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    }
                     setAllToInactive();
                 }
-                else {
-                    if (boldItalicIsActive) {
-                       currentLine.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), 0, currentLine.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        textChanged = true;
-                    } else if (italicIsActive) {
-                        currentLine.setSpan(new StyleSpan(Typeface.ITALIC), 0, currentLine.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        textChanged = true;
-                    } else if (boldIsActive) {
-                        currentLine.setSpan(new StyleSpan(Typeface.BOLD), 0, currentLine.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        textChanged = true;
-                    }
-                    if (titleIsActive) {
-                        currentLine.setSpan(new AbsoluteSizeSpan(CustomStyles.TITLE), 0, currentLine.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        textChanged = true;
-                    }
-                    if (subtitleIsActive) {
-                        currentLine.setSpan(new AbsoluteSizeSpan(CustomStyles.SUBTITLE), 0, currentLine.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        textChanged = true;
-                    }
-                }
-            }
-            if(selStart!= 0){
-                lastCursorPos = selStart;
-            }
-            if(textChanged) {
-                int[] bounds = getLineBoundaries(selStart, this);
-                CharSequence text = this.getText().replace(bounds[0], bounds[1], currentLine);
-                this.setText(text);
-                this.setSelection(selStart);
             }
         }
     }
