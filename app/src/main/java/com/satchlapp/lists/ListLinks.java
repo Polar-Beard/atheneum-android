e6f7c0478bd3af5d149a6090a802f960357e423b
Sara Montecino <saramontecino@outlook.com>
2017-02-28 14:34:23 -0800

    Update url for production

--- app/src/main/java/com/satchlapp/lists/ListLinks.java
+++ app/src/main/java/com/satchlapp/lists/ListLinks.java
@@ -5,8 +5,13 @@ package com.satchlapp.lists;
  */
 public class ListLinks {
     /** Production URL
-     * private static final String BASE_URL = "http://104.236.163.131:9000/api/";*/
-    public static final String BASE_URL = "http://192.168.1.68:9000/api/";
+     * private static final String BASE_URL = "http://104.236.163.131:9000/api/";
+     *
+     * Development URL
+     * private static final String BASE_URL = "http://192.168.1.68:9000/api/";
+     *
+     * */
+    private static final String BASE_URL = "http://104.236.163.131:9000/api/";
     public static final String API_GET_STORIES = BASE_URL + "story/n/100"; //Needs int num of stories
     public static final String API_USER_LOGIN = BASE_URL + "user/login";
     public static final String API_USER_REGISTER = BASE_URL + "user/register";
