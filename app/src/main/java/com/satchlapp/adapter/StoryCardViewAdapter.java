0786644d4f1c49d5f233f38dc6f9fc3064184c27
Sara Montecino <Sara Montecino>
2017-03-27 22:38:31 -0700

    Change animation on detailed story view

--- app/src/main/java/com/satchlapp/adapter/StoryCardViewAdapter.java
+++ app/src/main/java/com/satchlapp/adapter/StoryCardViewAdapter.java
@@ -3,6 +3,7 @@ package com.satchlapp.adapter;
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +11,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.provider.ContactsContract;
 import android.support.v4.graphics.ColorUtils;
 import android.support.v7.graphics.Palette;
 import android.support.v7.graphics.Target;
@@ -20,7 +22,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -101,12 +105,71 @@ public class StoryCardViewAdapter extends RecyclerView.Adapter<RecyclerView.View
                         }
                     });
             castedViewHolder.textViewTitle.setText(story.getTitle());
-            castedViewHolder.textViewDescriptionShort.setText(story.getDescription().substring(0, 150));
+            castedViewHolder.textViewDescriptionShort.setText(story.getDescription());
 
             castedViewHolder.buttonExpandDescription.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    View parentView = (View)view.getParent().getParent();
+                    ObjectAnimator raiseElevation = ObjectAnimator.ofFloat(
+                            castedViewHolder.cardViewButtonContainer,
+                            "cardElevation",
+                            30f);
+                    raiseElevation.setDuration(125);
+
+                    ObjectAnimator increaseMaxLines = ObjectAnimator.ofInt(
+                            castedViewHolder.textViewDescriptionShort,
+                            "maxLines",
+                            15);
+                    increaseMaxLines.setDuration(300);
+                    increaseMaxLines.setInterpolator(new DecelerateInterpolator());
+
+                    AnimatorSet animatorSet = new AnimatorSet();
+                    animatorSet.play(raiseElevation)
+                            .with(increaseMaxLines);
+                    animatorSet.start();
+
+                    float cardWidth = castedViewHolder.cardView.getWidth();
+                    float buttonWidth = castedViewHolder.linearLayoutButton.getWidth();
+                    final float distance = (cardWidth - buttonWidth)/2;
+                    castedViewHolder.linearLayoutButton.animate()
+                            .alpha(0f)
+                            .setDuration(150)
+                            .setListener(new Animator.AnimatorListener() {
+                                @Override
+                                public void onAnimationStart(Animator animator) {
+
+                                }
+
+                                @Override
+                                public void onAnimationEnd(Animator animator) {
+                                    castedViewHolder.linearLayoutButton.setTranslationX(-distance);
+                                    castedViewHolder.textViewButtonLabel.setText("Back");
+                                    castedViewHolder.buttonExpandDescription.setRotation(90f);
+                                    castedViewHolder.linearLayoutButton.animate()
+                                            .alpha(1f)
+                                            .setDuration(150);
+                                }
+
+                                @Override
+                                public void onAnimationCancel(Animator animator) {
+
+                                }
+
+                                @Override
+                                public void onAnimationRepeat(Animator animator) {
+
+                                }
+                            });
+
+                    castedViewHolder.buttonRead.setRotation(270f);
+                    castedViewHolder.linearLayoutContinue.setAlpha(0f);
+                    castedViewHolder.linearLayoutContinue.setVisibility(View.VISIBLE);
+                    castedViewHolder.linearLayoutContinue.animate()
+                            .alpha(1f)
+                            .setDuration(300);
+
+
+                    /*View parentView = (View)view.getParent().getParent();
                     View titleView = parentView.findViewById(R.id.listItemDetailedStoryTextViewTitle);
                     View descriptionView = parentView.findViewById(R.id.listItemDetailedStoryTextViewDescriptionShort);
                     int[] titleScreenLocation = new int[2];
@@ -128,7 +191,7 @@ public class StoryCardViewAdapter extends RecyclerView.Adapter<RecyclerView.View
                             putExtra("titleColor", ((ColorDrawable)titleView.getBackground()).getColor());
                     context.startActivity(subActivity);
 
-                    ((Activity)context).overridePendingTransition(0,0);
+                    ((Activity)context).overridePendingTransition(0,0);*/
                 }
             });
         } else if(viewHolder instanceof SimpleStoryViewHolder){
@@ -189,7 +252,12 @@ public class StoryCardViewAdapter extends RecyclerView.Adapter<RecyclerView.View
         public ImageView imageViewAuthorProfile;
         public TextView textViewDescriptionShort;
         public ImageView imageViewCoverImage;
-        public Button buttonExpandDescription;
+        public CardView cardViewButtonContainer;
+        public TextView textViewButtonLabel;
+        public ImageButton buttonExpandDescription;
+        public LinearLayout linearLayoutButton;
+        public LinearLayout linearLayoutContinue;
+        public ImageButton buttonRead;
 
         public DetailedStoryViewHolder(View itemView){
             super(itemView);
@@ -199,7 +267,12 @@ public class StoryCardViewAdapter extends RecyclerView.Adapter<RecyclerView.View
             imageViewAuthorProfile = (ImageView) itemView.findViewById(R.id.listItemDetailedStoryImageViewAuthorProfile);
             textViewDescriptionShort = (TextView) itemView.findViewById(R.id.listItemDetailedStoryTextViewDescriptionShort);
             imageViewCoverImage = (ImageView) itemView.findViewById(R.id.listItemDetailedStoryImageViewCoverImage);
-            buttonExpandDescription = (Button) itemView.findViewById(R.id.listItemDetailedStoryButtonExpandDescription);
+            textViewButtonLabel = (TextView) itemView.findViewById(R.id.listItemDetailedStoryTextViewButtonLabel);
+            cardViewButtonContainer = (CardView) itemView.findViewById(R.id.listItemDetailedStoryButtonContainer);
+            linearLayoutButton = (LinearLayout) itemView.findViewById(R.id.listItemDetailedStoryLinearLayoutButton);
+            linearLayoutContinue = (LinearLayout) itemView.findViewById(R.id.listItemDetailedStoryLinearLayoutContinue);
+            buttonRead = (ImageButton) itemView.findViewById(R.id.listItemDetailedStoryButtonRead);
+            buttonExpandDescription = (ImageButton) itemView.findViewById(R.id.listItemDetailedStoryButtonExpandDescription);
         }
     }
 
